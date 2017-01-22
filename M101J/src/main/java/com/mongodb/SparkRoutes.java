12d001aff35bb5fb971719547bc1517b47085a3d
Patrick Alex <patrickalex@gmail.com>
2017-01-22 15:39:26 -0200

    Add Spark.get "/" to SparkRoutes.java.

--- M101J/src/main/java/com/mongodb/SparkRoutes.java
+++ M101J/src/main/java/com/mongodb/SparkRoutes.java
@@ -1,9 +1,23 @@
 package com.mongodb;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
 public class SparkRoutes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Spark.get("/", new Route() {
+
+			@Override
+			public Object handle(final Request arg0, final Response arg1) throws Exception {
+				
+				return null;
+			}
+			
+		});
 
 	}
 
