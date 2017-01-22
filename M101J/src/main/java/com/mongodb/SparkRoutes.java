93c6a81f32ac0bebbe269d28a06824c8e924cc4b
Patrick Alex <patrickalex@gmail.com>
2017-01-22 15:42:05 -0200

    Add return to handle Override at Spark.get "/" in SparkRoutes.java

--- M101J/src/main/java/com/mongodb/SparkRoutes.java
+++ M101J/src/main/java/com/mongodb/SparkRoutes.java
@@ -10,13 +10,10 @@ public class SparkRoutes {
 	public static void main(String[] args) {
 		
 		Spark.get("/", new Route() {
-
 			@Override
 			public Object handle(final Request arg0, final Response arg1) throws Exception {
-				
-				return null;
+				return "Hello World from /!";
 			}
-			
 		});
 
 	}
