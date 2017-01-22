e0e7a047b084f748cc40f0730a1a860cb25825cb
Patrick Alex <patrickalex@gmail.com>
2017-01-21 23:02:32 -0200

    Adds Freemarker Templating Engine. Tested with HelloWorldFreemarkerStyle.java

---
+++ M101J/src/main/java/com/mongodb/HelloWorldFreemarkerStyle.java
@@ -0,0 +1,35 @@
+package com.mongodb;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class HelloWorldFreemarkerStyle {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("deprecation")
+		Configuration configuration = new Configuration();
+		// how to find the freemarker template file
+		configuration.setClassForTemplateLoading(
+				//resourceLoaderClass, basePackagePath
+				HelloWorldFreemarkerStyle.class, "/");
+				
+			try {
+				Template helloTemplate = configuration.getTemplate("hello.ftl");
+				StringWriter writer = new StringWriter();
+				Map<String, Object> helloMap = new HashMap<String, Object>();
+				helloMap.put("name", "Freemarker");
+				
+				helloTemplate.process(helloMap, writer);
+				
+				System.out.println(writer);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				
+			}
+	}
+}
