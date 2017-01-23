cee5bc5b709e4859e5969a2f598d4fc47187cf92
Patrick Alex <patrickalex@gmail.com>
2017-01-22 23:11:36 -0200

    Adds Freemarker configuration object to SparkFormHandling.java into src.

--- M101J/src/main/java/com/mongodb/SparkFormHandling.java
+++ M101J/src/main/java/com/mongodb/SparkFormHandling.java
@@ -1,9 +1,14 @@
 package com.mongodb;
 
+import freemarker.template.Configuration;
+
 public class SparkFormHandling {
 
 	public static void main(final String[] args) {
-		
+		// Configure Freemarker
+		@SuppressWarnings("deprecation")
+		final Configuration configuration = new Configuration();
+		configuration.setClassForTemplateLoading(SparkFormHandling.class, "/");
 	}
 
 }
