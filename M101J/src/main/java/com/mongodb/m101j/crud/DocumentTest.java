dab58822fe5d81d0894bef7a51a61c9b37d99e45
Patrick Alex Freitas da Silva <patrick.silva@qualirede.com.br>
2017-01-23 18:05:20 -0200

    Adds m101j/crud/DocumentTest.java and m101j/util/Helpers.java

---
+++ M101J/src/main/java/com/mongodb/m101j/crud/DocumentTest.java
@@ -0,0 +1,35 @@
+package com.mongodb.m101j.crud;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.m101j.util.Helpers.printJson;
+
+public class DocumentTest {
+
+	public static void main(String[] args) {
+		
+		Document document = new Document()
+				.append("str", "MongoDB, Hello")
+				.append("int", 42)
+				.append("l", 1L)
+				.append("double", 1.1)
+				.append("b", false)
+				.append("date", new Date())
+				.append("objectId", new ObjectId())
+				.append("null", null)
+				.append("embeddedDoc", new Document("x", 0))
+				.append("list", Arrays.asList(1, 2, 3));
+		
+		//String str = (String) document.getString("str");
+		printJson(document);
+		BsonDocument bsonDocument = new BsonDocument("str", new BsonString("MongoDB, Hello"));
+
+	}
+
+}
