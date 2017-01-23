cde7b2fb12d3ae2cb6a05c85e555ff85a9cda177
Patrick Alex <patrickalex@gmail.com>
2017-01-23 00:41:35 -0200

    Adds get "/" route to SparkHandling.class in src.

--- M101J/src/main/java/com/mongodb/SparkFormHandling.java
+++ M101J/src/main/java/com/mongodb/SparkFormHandling.java
@@ -1,6 +1,18 @@
 package com.mongodb;
 
+import static spark.Spark.halt;
+
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
 
 public class SparkFormHandling {
 
@@ -9,6 +21,25 @@ public class SparkFormHandling {
 		@SuppressWarnings("deprecation")
 		final Configuration configuration = new Configuration();
 		configuration.setClassForTemplateLoading(SparkFormHandling.class, "/");
+		
+		// Configure routes
+		Spark.get("/", new Route(){
+			@Override
+			public Object handle(final Request request, final Response response) throws Exception {
+				try {
+					final Map<String, Object> fruitsMap = new HashMap<String, Object>();
+					fruitsMap.put("fruits",
+							Arrays.asList("apple", "orange", "banana", "peach"));
+					final Template fruitPickerTemplate = configuration.getTemplate("fruitPicker.ftl");
+					final StringWriter writer = new StringWriter();
+					fruitPickerTemplate.process(fruitsMap, writer);
+					return writer;
+				} catch (final Exception e) {
+					halt(500);
+					return null;
+				}
+			}
+		});
 	}
 
 }
