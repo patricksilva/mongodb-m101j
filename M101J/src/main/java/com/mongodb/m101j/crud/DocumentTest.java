15f59334818fbe366fe941528093b1cd7fd29f18
Patrick Alex <patrickalex@gmail.com>
2017-02-05 02:40:52 -0200

    Adds a sample of how to create a BsonDocument

--- M101J/src/main/java/com/mongodb/m101j/crud/DocumentTest.java
+++ M101J/src/main/java/com/mongodb/m101j/crud/DocumentTest.java
@@ -1,20 +1,21 @@
 package com.mongodb.m101j.crud;
 
+import static com.mongodb.m101j.util.Helpers.printJson;
+
 import java.util.Arrays;
 import java.util.Date;
 
 import org.bson.BsonDocument;
+import org.bson.BsonInt64;
 import org.bson.BsonString;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
-import static com.mongodb.m101j.util.Helpers.printJson;
-
 public class DocumentTest {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		
-		Document document = new Document()
+		final Document document = new Document()
 				.append("str", "MongoDB, Hello")
 				.append("int", 42)
 				.append("l", 1L)
@@ -23,12 +24,22 @@ public class DocumentTest {
 				.append("date", new Date())
 				.append("objectId", new ObjectId())
 				.append("null", null)
+				// A Bson document can have nested documents, or lists
 				.append("embeddedDoc", new Document("x", 0))
-				.append("list", Arrays.asList(1, 2, 3));
+				.append("list", Arrays.asList(1, 2, 3)); // 1, 2, 3 could be any objects that the Document class supports. List of lists, list of documents, list of doubles or a mix of types if needed. 
 		
 		//String str = (String) document.getString("str");
 		printJson(document);
-		BsonDocument bsonDocument = new BsonDocument("str", new BsonString("MongoDB, Hello"));
+		
+		// Same thing could be done using a Type-safe based class:
+//		final BsonDocument bsonDocument = new BsonDocument("str", new BsonString("MongoDB, Hello"));
+		final BsonDocument bsonDocument = new BsonDocument()
+				.append("str", new BsonString("MongoDB, Hello"))
+				.append("int", new BsonInt64(1000));
+//				bsonDocument.append("int", new BsonInt64(1000));
+		
+		System.out.println(bsonDocument);
+		
 
 	}
 
